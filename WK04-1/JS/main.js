090d4ef6b2a7fbd63426ce1bac225eeecf1fec31
Jittrapol Intarasirisawat <jittrapol@gmail.com>
2024-02-14 13:38:49 +0700

    Added a function to ".ball"

--- WK04-1/JS/main.js
+++ WK04-1/JS/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
     $(".card").click(function(){
         console.log("click .card");
+        $(this).hide();
+    });
+
+
+    $(".ball").click(function(){
+        $(".info").hide();
     });
 });
